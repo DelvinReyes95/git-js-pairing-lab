a83f6aa67101a06c81b718e8e0ddd99fc8531d74
clopez11224 <christopherlopez000@gmail.com>
2022-03-23 18:05:47 +0000

    Solved Q2

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ function fiveToOneHundred(){
 }
 console.log("Q---1")
  fiveToOneHundred()
+ 
+ 
+ function multiplesOfThree() {
+     for (let i = 1; i <= 100; i++) {
+         if (i % 3 === 0) {
+             console.log(i)
+         }
+     }
+ }
+ console.log("Q---2");
+ multiplesOfThree()
